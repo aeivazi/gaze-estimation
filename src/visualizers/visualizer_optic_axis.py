7efc772f6264a60be93685e9563b2b45d4aff20e
aeivazi <anna.eivazi@gmail.com>
2017-12-04 12:40:54 +0100

    testing optic axis calculations

--- src/visualizers/visualizer_optic_axis.py
+++ src/visualizers/visualizer_optic_axis.py
@@ -1,12 +1,12 @@
 
 import numpy as np
+import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 from src.coordinate_system_transformations import transform_2D_to_3D
-from src.calculate_optic_axis import calculate_optic_axis_unit_vector
+from src.calculate_optic_axis import calculate_optic_axis_unit_vector, calculate_r, calculate_p
 from src.calculate_cornea_center import calculate_cornea_center
-from tests.point_of_interest_test import constants
 
 left_light_color = 'blue'
 right_light_color = 'red'
@@ -14,42 +14,45 @@ camera_color = 'yellow'
 left_glint_projection = 'blue'
 right_glint_projection = 'red'
 
-o_wcs_dummy = [0, 0, 0]
-l1_wcs_dummy = [-23, 0, 0]
-l2_wcs_dummy = [23, 0, 0]
-R = 0.9
+
+constants = {
+    'light_1_wcs': np.array([-23, 0, 0]),
+    'light_2_wcs': np.array([23, 0, 0]),
+    'camera_position_wcs': np.array([0, 0, 0]),
+    'focal_length_cm': 1.2,
+    'pixel_size_cm': (0.00048, 0.00048),
+    'principal_point': (400, 300),
+    'z_shift': -18,
+    'alpha_right': math.radians(-5),
+    'beta': math.radians(1.5),
+    'R_cm': 0.78,
+    'K_cm': 0.42,
+    'n1': 1.3375,
+    'n2': 1,
+    'distance_to_camera_cm': 52,
+    'camera_angle': math.radians(8)
+}
 
 
-def draw_line(point1, point2, magnitude_maltiply=1, color='b', marker='*', size=20):
+def draw_line(point1, point2, magnitude_maltiply=1, color='b', size=20):
 
     if magnitude_maltiply == 1:
         line_array = [point1, point2]
     else:
         unit_vector = (point2 - point1) / np.linalg.norm(point2 - point1)
         line_array = [point1, point1 + magnitude_maltiply* unit_vector]
-        print(unit_vector)
-        print(point1 + magnitude_maltiply* unit_vector)
 
     x, y, z = zip(*line_array)
     ax.plot(x, y, z, c=color)
-    ax.scatter(*(line_array[1]), c=color, marker=marker, s=size)
 
 
-def calculate_optic_axis(pupil_center_ics, cornea_center, **kwargs):
+def draw_unit_vector(unit_vector, starting_point=np.array([0, 0, 0]), magnitude_maltiply=1, color='b', size=20):
 
-    pupil_on_image_wgs = \
-        transform_2D_to_3D(*pupil_center_ics, kwargs['focal_length_cm'], *kwargs['pixel_size_cm'],
-                           *kwargs['principal_point'])
+    line_array = [starting_point, starting_point + magnitude_maltiply * unit_vector]
 
-    optic_axis_unit_vector = calculate_optic_axis_unit_vector(pupil_on_image_wgs,
-                                                              kwargs['camera_position_wcs'],
-                                                              cornea_center,
-                                                              kwargs['R_cm'],
-                                                              kwargs['K_cm'],
-                                                              kwargs['n1'],
-                                                              kwargs['n2'])
-
-    return optic_axis_unit_vector
+    x, y, z = zip(*line_array)
+    ax.plot(x, y, z, c=color)
+    ax.scatter(*(line_array[1]), c=color, marker='*', s=size)
 
 
 
@@ -62,31 +65,58 @@ if __name__ == '__main__':
     glint_2_ics = (309.05, 169.2)
     pupil_center_ics = (322.9, 173.3)
 
-    cornea_center = calculate_cornea_center(glint_1_ics, glint_2_ics, **constants)
+    # Cornea center of curvature in WCS
+    cornea_center_of_curvature_wcs = calculate_cornea_center(glint_1_ics, glint_2_ics, **constants)
 
-    optic_axis_unit_vector = calculate_optic_axis(pupil_center_ics, cornea_center, **constants)
+    # Pupil center on image in WCS
+    pupil_on_image_wcs = transform_2D_to_3D(*pupil_center_ics,
+                                            constants['focal_length_cm'],
+                                            *constants['pixel_size_cm'],
+                                            *constants['principal_point'])
 
-    print('Cornea center: {}'.format(cornea_center))
+    # Optic axis unit vector (iota)
+    optic_axis_unit_vector = calculate_optic_axis_unit_vector(pupil_on_image_wcs,
+                                                              constants['camera_position_wcs'],
+                                                              cornea_center_of_curvature_wcs,
+                                                              constants['R_cm'],
+                                                              constants['K_cm'],
+                                                              constants['n1'],
+                                                              constants['n2'])
+
+    print('Pupil wcs {}'.format(pupil_on_image_wcs))
+    print('Cornea center: {}'.format(cornea_center_of_curvature_wcs))
     print('Optic axis unit vector: {}'.format(optic_axis_unit_vector))
 
     ax.scatter(*constants['camera_position_wcs'], c=camera_color, marker='*', s=50)
     ax.scatter(*constants['light_1_wcs'], c=left_light_color, marker='v', s=50)
 
-    # ax.scatter(*cornea_center, c=left_glint_projection, marker='o', s=50)
-    #
-    ax.scatter(*optic_axis_unit_vector, c=left_light_color, marker='*', s=50)
-    # draw_line(constants['camera_position_wcs'], optic_axis_unit_vector, magnitude_maltiply=1, color=left_light_color, marker='o', size=50)
-    draw_line(cornea_center, cornea_center+optic_axis_unit_vector, magnitude_maltiply=50, color=left_light_color,
-              marker='*', size=50)
+    ax.scatter(*cornea_center_of_curvature_wcs, c=left_glint_projection, marker='o', s=50)
 
-    # ax.scatter(*q, c=left_glint_projection, marker='*', s=50)
+    draw_unit_vector(optic_axis_unit_vector, starting_point=cornea_center_of_curvature_wcs, magnitude_maltiply=1, color='red')
 
-    # draw_line(q, c, magnitude_maltiply=1, color=left_light_color, marker='o', size=50)
+
+    ### Intermidiate points and unit vectors
+
+    # # Pupil point of refraction(r) in WCS
+    pupil_point_of_refraction_wcs = calculate_r(constants['camera_position_wcs'],
+                                                pupil_on_image_wcs,
+                                                cornea_center_of_curvature_wcs,
+                                                constants['R_cm'])
+
+    pupil_center_wcs = \
+        calculate_p(constants['camera_position_wcs'],
+                    pupil_point_of_refraction_wcs,
+                    cornea_center_of_curvature_wcs,
+                    constants['R_cm'], constants['K_cm'], constants['n1'], constants['n2'])
+
+    ax.scatter(*pupil_point_of_refraction_wcs, c='blue', marker='*', s=50)
+    ax.scatter(*pupil_center_wcs, c='blue', marker='*', s=50)
+    draw_line(constants['camera_position_wcs'], pupil_point_of_refraction_wcs)
 
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
 
-    ax.view_init(elev=96, azim=90)
+    ax.view_init(elev=10, azim=12)
 
     plt.show()
